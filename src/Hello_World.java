f6d8d84495077ce56fdc1656e7cc75076bc917ac
Zebo1985 <zebo0585@gmail.com>
2020-08-01 12:08:18 -0400

    add Hello_World.java

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -2,5 +2,8 @@ public class Hello_World {
     public static void main(String[] args) {
         System.out.println("Hello gitHub");
         System.out.println("Hello git commit");
+        //I can do it
+        // you also can do it
+
     }
 }
